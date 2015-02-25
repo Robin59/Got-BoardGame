21f4e2485502585a1c65da4eee19219a18963ee3
Robin59 <robingiraudon@hotmail.fr>
2015-02-25 22:48:53 +0100

    Update the tests for being compatible with battle class

--- test/src/org/jogre/gameOfThrones/common/Battle/BattleTest.java
+++ test/src/org/jogre/gameOfThrones/common/Battle/BattleTest.java
@@ -1,6 +1,11 @@
 package org.jogre.gameOfThrones.common.Battle;
+import java.awt.Label;
+
+import javax.swing.JLabel;
+
 import org.jogre.gameOfThrones.common.CombatantCard;
 import org.jogre.gameOfThrones.common.Family;
+import org.jogre.gameOfThrones.common.GameOfThronesModel;
 import org.jogre.gameOfThrones.common.combat.*;
 import org.jogre.gameOfThrones.common.orders.Order;
 import org.jogre.gameOfThrones.common.orders.OrderType;
@@ -14,6 +19,7 @@ import static org.junit.Assert.*;
 public class BattleTest {
 	@Test
 	 public void constructor() {
+		GameOfThronesModel model = new GameOfThronesModel(3, new JLabel() );
 		//family construction
 		Family attFamily = new Family(1, null);
 		Family defFamily = new Family(2, null);
@@ -25,8 +31,8 @@ public class BattleTest {
 		defTerritory.setTroup(new GroundForce(defFamily,1,0,0));
 		neutralTerritory.setNeutralForce(1);
 		//
-		Battle battlePvP=new BattlePvP(attTerritory,defTerritory);
-		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory);
+		Battle battlePvP=new BattlePvP(attTerritory,defTerritory,model,attTerritory.getOrder());
+		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory,model,attTerritory.getOrder());
 		assertNotNull(battlePvP);
 		assertNotNull(battlePvE);
 		assertEquals(battlePvP.getAttFamily(),attFamily);
@@ -35,6 +41,7 @@ public class BattleTest {
 	
 	@Test
 	 public void initialForceCalulTestPvP() {
+		GameOfThronesModel model = new GameOfThronesModel(3, new JLabel() );
 		Family attFamily = new Family(1, null);
 		Family defFamily = new Family(2, null);
 		//contruction territory 
@@ -43,7 +50,7 @@ public class BattleTest {
 		attTerritory.setTroup(new GroundForce(attFamily,1,0,1));
 		attTerritory.setOrder(new Order(false,0,0,OrderType.ATT));
 		defTerritory.setTroup(new GroundForce(defFamily,1,0,1));
-		Battle battle=new BattlePvP(attTerritory,defTerritory);
+		Battle battle=new BattlePvP(attTerritory,defTerritory,model,attTerritory.getOrder());
 		//test defender force
 		assertEquals(battle.defPower(),3);
 		defTerritory.setOrder(new Order(false,0,0,OrderType.CON));
@@ -64,13 +71,14 @@ public class BattleTest {
 	
 	@Test
 	public void initialForceCalulTestPvE() {
+		GameOfThronesModel model = new GameOfThronesModel(3, new JLabel() );
 		Family attFamily = new Family(1, null);
 		Territory attTerritory= new Land("test",0,0,0);
 		Territory neutralTerritory= new Land("test",0,0,0);
 		attTerritory.setTroup(new GroundForce(attFamily,1,0,0));
 		attTerritory.setOrder(new Order(true, 0, 1, OrderType.ATT));
 		neutralTerritory.setNeutralForce(3);
-		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory);
+		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory,model,attTerritory.getOrder());
 		//support 
 		Territory supTerritory= new Land("test",0,0,0);
 		supTerritory.setTroup(new GroundForce(attFamily,1,0,0));
@@ -93,13 +101,14 @@ public class BattleTest {
 	
 	@Test
 	public void PveTest(){
+		GameOfThronesModel model = new GameOfThronesModel(3, new JLabel() );
 		Family attFamily = new Family(1, null);
 		Territory attTerritory= new Land("test",0,0,0);
 		Territory neutralTerritory= new Land("test",0,0,0);
 		attTerritory.setTroup(new GroundForce(attFamily,1,1,1));
 		attTerritory.setOrder(new Order(true, 0, 1, OrderType.ATT));
 		neutralTerritory.setNeutralForce(3);
-		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory);
+		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory,model,attTerritory.getOrder());
 		battlePvE.addTroop(0, 1, 0, 1);
 		// defeat test
 		battlePvE.startBattle();
@@ -109,7 +118,7 @@ public class BattleTest {
 		assertEquals(attTerritory.getTroup().getTroops()[3],0);
 		assertEquals(neutralTerritory.getNeutralForce(),3);
 		//victory test
-		Battle battle2=new BattlePvE(attTerritory,neutralTerritory);
+		Battle battle2=new BattlePvE(attTerritory,neutralTerritory,model,attTerritory.getOrder());
 		battle2.addTroop(0, 0, 1, 0);
 		battle2.startBattle();
 		assertTrue(attTerritory.getOrder().getUse());
@@ -125,13 +134,14 @@ public class BattleTest {
 	 
 	@Test
 	public void supportTest(){
+		GameOfThronesModel model = new GameOfThronesModel(3, new JLabel() );
 		Family attFamily = new Family(1, null);
 		Territory attTerritory= new Land("test",0,0,0);
 		Territory neutralTerritory= new Land("test",0,0,0);
 		attTerritory.setTroup(new GroundForce(attFamily,1,0,0));
 		attTerritory.setOrder(new Order(true, 0, 1, OrderType.ATT));
 		neutralTerritory.setNeutralForce(4);
-		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory);
+		Battle battlePvE=new BattlePvE(attTerritory,neutralTerritory,model,attTerritory.getOrder());
 		battlePvE.addTroop(0, 1, 0, 0);
 		//support 
 		Territory supTerritory= new Land("test",0,0,0);
@@ -146,6 +156,7 @@ public class BattleTest {
 	}
 	@Test
 	public void swordTest(){
+		GameOfThronesModel model = new GameOfThronesModel(3, new JLabel() );
 		Family attFamily = new Family(1, null);
 		Family defFamily = new Family(2, null);
 		attFamily.setFiefdomsTrack(1);
@@ -156,7 +167,7 @@ public class BattleTest {
 		attTerritory.setOrder(new Order(false,0,0,OrderType.ATT));
 		defTerritory.setTroup(new GroundForce(defFamily,1,0,0));
 		defTerritory.setOrder(new Order(true,2,0,OrderType.DEF));
-		Battle battle=new BattlePvP(attTerritory,defTerritory);
+		Battle battle=new BattlePvP(attTerritory,defTerritory,model,attTerritory.getOrder());
 		battle.addTroop(0,1,0,0);
 		battle.startBattle();
 		battle.playCard(new CombatantCard("", 1, 0, 0), attFamily);
@@ -166,6 +177,7 @@ public class BattleTest {
 	
 	@Test
 	public void cardsTest(){
+		GameOfThronesModel model = new GameOfThronesModel(3, new JLabel() );
 		Family attFamily = new Family(1, null);
 		Family defFamily = new Family(2, null);
 		attFamily.setFiefdomsTrack(1);
@@ -176,7 +188,7 @@ public class BattleTest {
 		attTerritory.setOrder(new Order(false,0,0,OrderType.ATT));
 		defTerritory.setTroup(new GroundForce(defFamily,1,0,0));
 		defTerritory.setOrder(new Order(true,2,0,OrderType.DEF));
-		Battle battle=new BattlePvP(attTerritory,defTerritory);
+		Battle battle=new BattlePvP(attTerritory,defTerritory,model,attTerritory.getOrder());
 		battle.addTroop(0,1,0,0);
 		assertEquals(battle.getState(), Battle.BATTLE_SUPPORT_PHASE);
 		battle.startBattle();
