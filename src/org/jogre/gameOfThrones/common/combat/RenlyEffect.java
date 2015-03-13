7de3000e352ff77877267cb0a497bc280c8adef5
Robin59 <robingiraudon@hotmail.fr>
2015-03-13 02:56:21 +0100

    Add a small correction for Renly card's effect (improve the condition for using it)

--- src/org/jogre/gameOfThrones/common/combat/RenlyEffect.java
+++ src/org/jogre/gameOfThrones/common/combat/RenlyEffect.java
@@ -11,11 +11,11 @@ public class RenlyEffect extends HouseCardEffect {
 		battle.setState(Battle.BATTLE_CARD_EFFECT_END_BATTLE);
 	}
 
-	@Override 
+	@Override
 	public void execute(Territory territory) {
-		//check if the territory is own by the good player, if there is a footman 
+		//check if the territory is own by the good player, if there is a footman and if the footman participated to the battle
 		if(territory.getFamily()!=null && territory.getFamily()==playerFamily &&
-				territory.getTroup()!=null && territory.getTroup().getTroops()[1]>0){
+				territory.getTroup()!=null && territory.getTroup().getTroops()[1]>0 && inBattle(territory)){
 			//then change a footman in knight
 			territory.getTroup().addToop(0, 0, 1, 0);
 			territory.getTroup().rmToop(0, 1, 0, 0);
@@ -39,6 +39,21 @@ public class RenlyEffect extends HouseCardEffect {
 		}else return 0;
 	}
 	
+	/*Return if the territory took part of the battle (in the player camp)*/
+	private boolean inBattle(Territory territory){
+		if(defender){
+			if(territory==battle.getDefTerritory()) return true;
+			for(Territory t : battle.defSupport){
+				if(territory==t) return true;
+			}
+		}else{
+			if(territory==battle.getAttTerritory()) return true;
+			for(Territory t : battle.attSupport){
+				if(territory==t) return true;
+			}
+		}
+		return false;
+	}
 
 	@Override
 	public void autoExecute() {}
