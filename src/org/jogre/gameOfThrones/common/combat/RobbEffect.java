84172e709500948da7dd5c045ad6d69cff727082
Robin59 <robingiraudon@hotmail.fr>
2015-03-15 01:47:34 +0100

    Add the attacker withdraw in case Robb Stark is played by the defender

--- src/org/jogre/gameOfThrones/common/combat/RobbEffect.java
+++ src/org/jogre/gameOfThrones/common/combat/RobbEffect.java
@@ -1,5 +1,8 @@
 package org.jogre.gameOfThrones.common.combat;
 
+import java.util.List;
+
+import org.jogre.gameOfThrones.common.territory.Land;
 import org.jogre.gameOfThrones.common.territory.Territory;
 import org.jogre.gameOfThrones.common.territory.Water;
 
@@ -10,8 +13,10 @@ public class RobbEffect extends HouseCardEffect {
 		if(defender){
 			//suppress the siege machines of the battle looser
 			battle.getAttTroops()[3]=0;
-			// if there is no place to withdraw or no more troops, don't do anything
-			//...
+			// if there is no more troops, don't do anything
+			if(battle.getAttTroops()[0]>0 || battle.getAttTroops()[1]>0 || battle.getAttTroops()[2]>0){
+				battle.setState(Battle.BATTLE_CARD_EFFECT_END_BATTLE);
+			}else finish=true;
 		}else{
 			//suppress the siege machines of the battle looser
 			battle.getDefTerritory().getTroup().getTroops()[3]=0;
@@ -25,10 +30,26 @@ public class RobbEffect extends HouseCardEffect {
 
 	@Override
 	public void execute(Territory territory) {
-		if(battle.defTerritory.canWithdraw(territory)){
-			battle.getDefTerritory().mouveTroops(territory);
-			battle.getDefTerritory().setTroup(new NavalTroup(oppFamily, 0));
-			finish=true;
+		if(defender){
+			if(attCanWithdraw(territory)){
+				if(territory.getTroup()!=null){
+					territory.getTroup().addTroop(battle.attTroops);
+				}else{
+					if(territory instanceof Water){
+						territory.setTroup(new NavalTroup(oppFamily, battle.getAttTroops()[0]));
+					}else{
+						territory.setTroup(new GroundForce(oppFamily, battle.getAttTroops()[1],battle.getAttTroops()[2],0));
+					}
+				}
+				for(int i=0;i<3;i++) battle.getAttTroops()[i]=0;
+				finish=true;
+			}
+		}else{
+			if(battle.defTerritory.canWithdraw(territory)){
+				battle.getDefTerritory().mouveTroops(territory);
+				battle.getDefTerritory().setTroup(new NavalTroup(oppFamily, 0));
+				finish=true;
+			}
 		}
 	}
 
@@ -43,5 +64,19 @@ public class RobbEffect extends HouseCardEffect {
 		return 0;
 	}
 
+	private boolean attCanWithdraw(Territory territory){
+		if(battle.defTerritory.getNeighbors().contains(territory)){
+			if(battle.defTerritory instanceof Water){
+				return (territory instanceof Water) && (territory.getFamily()==null || territory.getFamily()==oppFamily);
+			}else{
+				return (territory instanceof Land) && (territory.getFamily()==null || territory.getFamily()==oppFamily);
+			}
+		}else{
+			return false;//or naval withdraw
+		}
+	}
+	
+	
+	
 	
 }
