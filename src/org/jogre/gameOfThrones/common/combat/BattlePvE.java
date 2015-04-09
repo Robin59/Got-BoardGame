1075c36ff27f88517b22c19c9ffb64a820e827ab
robin59 <robingiraudon@hotmail.fr>
2015-04-09 18:14:27 +0200

    Made some corrections and simplifications in the class BattlePvE

--- src/org/jogre/gameOfThrones/common/combat/BattlePvE.java
+++ src/org/jogre/gameOfThrones/common/combat/BattlePvE.java
@@ -3,9 +3,7 @@ package org.jogre.gameOfThrones.common.combat;
 import org.jogre.gameOfThrones.common.Family;
 import org.jogre.gameOfThrones.common.GameOfThronesModel;
 import org.jogre.gameOfThrones.common.orders.Order;
-import org.jogre.gameOfThrones.common.orders.OrderType;
 import org.jogre.gameOfThrones.common.territory.Territory;
-import org.jogre.gameOfThrones.common.territory.Water;
 
 public class BattlePvE extends Battle{
 	
@@ -24,8 +22,7 @@ public class BattlePvE extends Battle{
 	public int defPower(){
 		int res= defTerritory.getNeutralForce();
 		for(Territory territory : defSupport){
-			int[] troops =territory.getTroup().getTroops();
-			res+=territory.getOrder().getOthBonus()+troops[0]+troops[1]+troops[2]*2;
+			res+=territory.getOrder().getOthBonus()+territory.getTroup().getDefPower();
 		}
 		return res;
 	}
@@ -57,10 +54,9 @@ public class BattlePvE extends Battle{
 	public boolean canPlayCard(Family family) {return false;}
 	
 	
-	
 	///////old class
 	public void resolution(GameOfThronesModel model) {
-		if(victory()){
+		if(attPower()>=defPower()){
 			defTerritory.setNeutralForce(0);
 			defTerritory.setTroup(new GroundForce(attTerritory.getFamily(),attTroops[1],attTroops[2],attTroops[3]));
 			if(attTerritory.getTroup()== null || attTerritory.getTroup().getEffectif()==0){//if there is no more troops the territory does not belong to the player any more
@@ -77,18 +73,7 @@ public class BattlePvE extends Battle{
 		}
 	}
 	
-	private boolean victory(){
-		int res=attOrder.getOthBonus();
-		res+=(attTroops[2])*2+attTroops[1]+attTroops[3]*4*groundType;
-		//support
-		for(Territory territory : defTerritory.getNeighbors()){
-			if(territory.getOrder()!=null && territory.getOrder().getType()==OrderType.SUP && territory.getFamily()==attTerritory.getFamily() ){
-				int[] troops =territory.getTroup().getTroops();
-				res+=territory.getOrder().getOthBonus()+troops[0]+troops[1]+troops[2]*2+troops[3]*4*groundType;
-			}
-		}
-		return res>=defTerritory.getNeutralForce();
-	}
+	
 
 
 
